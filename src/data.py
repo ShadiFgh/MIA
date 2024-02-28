d297841801bd24a34b464c1f16b2aebb66a7a2b3
Shadi FarokhGhate <56694398+ShadiFgh@users.noreply.github.com>
2024-02-28 12:39:39 -0800

    Update data.py

---
+++ src/data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from datasets import load_dataset
+
+
+
+def get_dataset():
+
+  dataset = load_dataset('ag_news')
+  df_test = pd.DataFrame( dataset['test'] )
+  df_test = df_test.dropna()
+  df_train = pd.DataFrame( dataset['train'] )
+  df_train = df_train.dropna()
+
+  return df_train, df_test
